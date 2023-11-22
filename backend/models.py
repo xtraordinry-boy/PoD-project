8ed5660fb77c67175a9512d0818507914839cff7
Aditya Sahu <119127013+xtraordinry-boy@users.noreply.github.com>
2023-11-22 22:42:01 +0530

    Update models.py
    code edited

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,26 @@
+from django.db import models
+
+class User(models.Model):
+  username = models.CharField(max_length=255, unique=True)
+  email = models.EmailField(unique=True)
+  password = models.CharField(max_length=255)
+
+class Store(models.Model):
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=255)
+  description = models.TextField()
+
+class Design(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  store = models.ForeignKey(Store, on_delete=models.CASCADE)
+  name = models.CharField(max_length=255)
+  description = models.TextField()
+  image = models.ImageField(upload_to='designs')
+
+class Order(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  design = models.ForeignKey(Design, on_delete=models.CASCADE)
+  product_type = models.CharField(max_length=255) # e.g., T-shirt, mug, phone case
+  status = models.CharField(max_length=255) # e.g., pending, processing, shipped, delivered
+  order_date = models.DateTimeField(auto_now_add=True)
 
