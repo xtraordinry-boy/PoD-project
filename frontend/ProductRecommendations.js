4fc00c0db2ad51400674e5d86d079618d2a3e712
Aditya Sahu <119127013+xtraordinry-boy@users.noreply.github.com>
2023-11-22 22:40:08 +0530

    Update ProductRecommendations.js
    code edited

--- frontend/ProductRecommendations.js
+++ frontend/ProductRecommendations.js
@@ -1,1 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const ProductRecommendations = () => {
+  const [recommendations, setRecommendations] = useState([]);
+
+  useEffect(() => {
+    // Fetch product recommendations using machine learning algorithms
+    const fetchRecommendations = async () => {
+      const response = await fetch('/api/recommendations');
+      const data = await response.json();
+      setRecommendations(data);
+    };
+    fetchRecommendations();
+  }, []);
+
+  return (
+    <div className="product-recommendations">
+      <h2>Recommended Products for You</h2>
+      {recommendations.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+
+  );
+};
+
+export default ProductRecommendations;
 
