28fde7516fd972c3dca87a47a95bd54418858d9a
Aditya Sahu <119127013+xtraordinry-boy@users.noreply.github.com>
2023-11-22 22:54:04 +0530

    Update urls.py
    code edited

--- backend/urls.py
+++ backend/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.user_registration, name='user_registration'),
+    path('create-store/', views.store_creation, name='store_creation'),
+    path('upload-design/', views.design_upload, name='design_upload'),
+    path('get-designs/', views.retrieve_designs, name='retrieve_designs'),
+    path('delete-design/', views.delete_design, name='delete_design'),
+]
 
