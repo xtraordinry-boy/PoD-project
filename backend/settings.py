a1164691da739c029026bb6b41ed220531d8c808
Aditya Sahu <119127013+xtraordinry-boy@users.noreply.github.com>
2023-11-22 22:54:56 +0530

    Update settings.py
    code edited

--- backend/settings.py
+++ backend/settings.py
@@ -1,1 +1,47 @@
+import os
+from pathlib import Path
+
+# Build paths inside the project like: os.path.join(BASE_DIR, ...)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: Keep this key in secret!
+SECRET_KEY = 'YOUR_SECRET_KEY'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/ref/settings/#static-files
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-primary-key-field-type
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS (Cross-Origin Resource Sharing) settings
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE'
+]
+CORS_ALLOW_HEADERS = [
+    'Content-Type',
+    'Authorization',
+]
 
