676fa6de25329d29b664d69ae6c19a02215ca154
Aditya Sahu <119127013+xtraordinry-boy@users.noreply.github.com>
2023-11-22 22:38:33 +0530

    Update Homepage.js
    code edited

--- frontend/Homepage.js
+++ frontend/Homepage.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ProductList from './ProductList';
+import ProductRecommendations from './ProductRecommendations';
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to the Print-on-Demand Platform</h1>
+      <ProductRecommendations />
+      <ProductList />
+    </div>
+  );
+};
+
+export default Homepage;
 
