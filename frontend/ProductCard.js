bbeee02386232b120c327a64454690360c2c8db5
Aditya Sahu <119127013+xtraordinry-boy@users.noreply.github.com>
2023-11-22 22:39:02 +0530

    Update ProductCard.js
    code edited

--- frontend/ProductCard.js
+++ frontend/ProductCard.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className="product-card">
+      <Link to={`/products/${product.id}`}>
+        <img src={product.image} alt={product.name} />
+      </Link>
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <span>${product.price}</span>
+    </div>
+  );
+};
+
+export default ProductCard;
 
